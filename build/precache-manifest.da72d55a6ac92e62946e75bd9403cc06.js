2f047a13af2746387e6cb3ece08eace67696e0a6
Christopher Conaty <chrisconaty@gmail.com>
2022-10-06 23:52:40 -0400

    second commit

---
+++ build/precache-manifest.da72d55a6ac92e62946e75bd9403cc06.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "9f5a94f6b3cc44478d8f",
+    "url": "/static/css/main.e7149d58.chunk.css"
+  },
+  {
+    "revision": "9f5a94f6b3cc44478d8f",
+    "url": "/static/js/main.9f5a94f6.chunk.js"
+  },
+  {
+    "revision": "aa1ef92c5cf7728f9d0a",
+    "url": "/static/js/1.aa1ef92c.chunk.js"
+  },
+  {
+    "revision": "e7e9aadb20c6e7ce6495",
+    "url": "/static/css/2.1942b7d4.chunk.css"
+  },
+  {
+    "revision": "e7e9aadb20c6e7ce6495",
+    "url": "/static/js/2.e7e9aadb.chunk.js"
+  },
+  {
+    "revision": "3ea3b0ffbd7375107cfd",
+    "url": "/static/js/runtime~main.3ea3b0ff.js"
+  },
+  {
+    "revision": "865c964e6682ee8d045a24757569cae5",
+    "url": "/static/media/logo.865c964e.svg"
+  },
+  {
+    "revision": "2742d81afb69e902e4513dc7cdda0a7f",
+    "url": "/static/media/roboto-cyrillic-ext-500-normal.2742d81a.woff2"
+  },
+  {
+    "revision": "7b08b9e11fc6b8a8a1398b357e874144",
+    "url": "/static/media/roboto-cyrillic-500-normal.7b08b9e1.woff2"
+  },
+  {
+    "revision": "53f395eb854a40e978706b1082570e42",
+    "url": "/static/media/roboto-greek-500-normal.53f395eb.woff2"
+  },
+  {
+    "revision": "e7b7001dff6c14165abdc0fefdecae06",
+    "url": "/static/media/roboto-greek-ext-500-normal.e7b7001d.woff2"
+  },
+  {
+    "revision": "7cda2cfee99d697daf8c14819d9004eb",
+    "url": "/static/media/roboto-vietnamese-500-normal.7cda2cfe.woff2"
+  },
+  {
+    "revision": "68d75d959b2a0e9958b11d781338c8f7",
+    "url": "/static/media/roboto-all-500-normal.68d75d95.woff"
+  },
+  {
+    "revision": "e36fccd06262bef92e7a9841e2202225",
+    "url": "/static/media/roboto-latin-ext-500-normal.e36fccd0.woff2"
+  },
+  {
+    "revision": "3a44e06eb954b96aa043227f3534189d",
+    "url": "/static/media/roboto-latin-500-normal.3a44e06e.woff2"
+  },
+  {
+    "revision": "9d0814e545df3e9d47cba1060be3f2e5",
+    "url": "/index.html"
+  }
+];
--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
+import '@fontsource/roboto/500.css';
 
+ 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Chris Did something. No Progress will be made for a while.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button   href="https://youtube.com/shorts/iavN1tmY62o?feature=share"
+          target="_blank" rel="noopener noreferrer" variant="contained" color="primary">
+        I am a Videographer. 
+      </Button>
+       
       </header>
     </div>
   );
